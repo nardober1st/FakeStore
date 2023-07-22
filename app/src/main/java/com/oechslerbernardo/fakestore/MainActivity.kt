4bfe5c1902ca8ac914a2aada9314c9d5e00bd4e1
nardober1st <bernardo.oechsler6@gmail.com>
2023-07-21 22:54:50 -0700

    add funs to main

--- app/src/main/java/com/oechslerbernardo/fakestore/MainActivity.kt
+++ app/src/main/java/com/oechslerbernardo/fakestore/MainActivity.kt
@@ -3,10 +3,13 @@ package com.oechslerbernardo.fakestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
+import coil.load
+import com.oechslerbernardo.fakestore.databinding.ActivityMainBinding
+import com.oechslerbernardo.fakestore.hilt.service.ProductsService
 import dagger.hilt.android.AndroidEntryPoint
-import retrofit2.Response
-import retrofit2.http.GET
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -15,19 +18,57 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var productsService: ProductsService
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        refreshData()
+        setupListeners()
+    }
+
+    private fun refreshData() {
         lifecycleScope.launchWhenStarted {
+            binding.productImageViewLoadingProgressBar.isVisible = true
             val response = productsService.getAllProducts()
-            Log.i("TAGY", response.body()!!.toString())
+            binding.productImageView.load(
+                data = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+            ) {
+                listener { request, result ->
+                    binding.productImageViewLoadingProgressBar.isGone = true
+                }
+            }
+            Log.i("DATA", response.body()!!.toString())
         }
     }
 
-    interface ProductsService {
+    private fun setupListeners() {
+        // If its visible, when we click, it becomes invisible and vice versa
+        binding.cardView.setOnClickListener {
+            binding.productDescriptionTextView.apply {
+                isVisible = !isVisible
+            }
+        }
 
-        @GET("products")
-        suspend fun getAllProducts(): Response<List<Any>>
+        // If its visible, when we click, it becomes invisible and vice versa
+        binding.addToCartButton.setOnClickListener {
+            binding.inCartView.apply {
+                isVisible = !isVisible
+            }
+        }
+
+        // check if its favorite or not and display the corresponding drawable on the UI
+        var isFavorite = false
+        binding.favoriteImageView.setOnClickListener {
+            val imageRes = if (isFavorite) {
+                R.drawable.ic_round_favorite_border_24
+            } else {
+                R.drawable.ic_round_favorite_24
+            }
+            binding.favoriteImageView.setIconResource(imageRes)
+            isFavorite = !isFavorite
+        }
     }
 }
